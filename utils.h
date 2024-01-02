fdcb62231663a0d136a8af689e7d76dc45cf073f
kyuuzou1125 <73418628+kyuuzou1125@users.noreply.github.com>
2024-01-02 14:52:41 +0800

    Add files via upload

---
+++ utils.h
@@ -0,0 +1,15 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+#include <cmath>
+#include <base_local_planner/local_planner_util.h>
+
+//#define degToRad(Deg) ((Deg) * M_PI / 180.0)
+//#define radToDeg(Rad) ((Rad) * 180.0 / M_PI)
+
+double getCoordinateAngle(int x, int y);
+double getCoordinateDistance(int x, int y);
+double degToRad(double deg);
+double radToDeg(double rad);
+
+#endif
